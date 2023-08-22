50fa2c9b49c6ec6edfbd0dbade55a7a15da7bdf4
Khwaja Abdul Samad <thatsme104@gmail.com>
2023-08-23 00:15:13 +0530

    First Commit

---
+++ packages_npm.js
@@ -0,0 +1,58 @@
+//Required package
+var pdf = require("pdf-creator-node");
+var fs = require("fs");
+
+// Read HTML Template
+var html = fs.readFileSync("96_templates.html", "utf8");
+
+var options = {
+    format: "A3",
+    orientation: "portrait",
+    border: "10mm",
+    header: {
+        height: "45mm",
+        contents: '<div style="text-align: center;">Author: Outlier</div>'
+    },
+    footer: {
+        height: "28mm",
+        contents: {
+            first: 'Cover page',
+            2: 'Second page', // Any page number is working. 1-based index
+            default: '<span style="color: #444;">{{page}}</span>/<span>{{pages}}</span>', // fallback value
+            last: 'Last Page'
+        }
+    }
+};
+
+
+var users = [
+    {
+      name: "Khwaja",
+      age: "26",
+    },
+    {
+      name: "Abdul",
+      age: "26",
+    },
+    {
+      name: "Samad",
+      age: "26",
+    },
+  ];
+  var document = {
+    html: html,
+    data: {
+      users: users,
+    },
+    path: "./output.pdf",
+    type: "pdf",
+  };
+
+  pdf
+  .create(document, options)
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
